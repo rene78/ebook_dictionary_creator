873658ac3ffb501f3bff851acd03bc5da449db52
rene78 <36115705+rene78@users.noreply.github.com>
2025-08-16 16:17:49 +0800

    raw glosses do not seem to be available anymore

--- create-dictionary.py
+++ create-dictionary.py
@@ -2,7 +2,7 @@ from ebook_dictionary_creator import DictionaryCreator
 
 dict_creator = DictionaryCreator("Malay", "English")#Source language, target language
 dict_creator.download_data_from_kaikki()
-dict_creator.create_database()
+dict_creator.create_database(use_raw_glosses=False)# No more raw glosses in the downloaded data from Kaikki.
 dict_creator.export_to_tabfile()
 dict_creator.export_to_kindle(
     kindlegen_path="/Applications/Kindle Previewer 3.app/Contents/lib/fc/bin/kindlegen",# Path to kindlegen executable
